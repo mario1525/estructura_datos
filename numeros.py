374a9068977d89b5c2149982c4217d91fd83e198
mario1525 <mbeltran4892@gmail.com>
2022-11-09 13:17:09 -0500

    avances en el proyecto

---
+++ numeros.py
@@ -0,0 +1,52 @@
+
+
+
+
+def conver(N):
+
+    D = N/10
+
+    U = N -(D*10)
+
+    if D == 1:
+     print('X')
+    if D == 2:
+     print('XX')
+    if D == 3:
+     print('XXX')
+    if D == 4:
+     print('XL')
+    if D == 5:
+      print('L')
+    if D == 6:
+      print('LX')
+    if D == 7:
+     print('LXX')
+    if D == 8:
+      print('LXXX')
+    if D == 9:
+     print('XD')
+    if D == 10:
+     print('D')
+
+    if U == 1:
+     print('I')
+    if U == 2:
+     print('II')
+    if U == 3:
+     print('III')
+    if U == 4:
+     print('IV')
+    if U == 5:
+      print('V')
+    if U == 6:
+      print('VI')
+    if U == 7:
+     print('VII')
+    if U == 8:
+      print('VIII')
+    if U == 9:
+     print('IX') 
+
+
+conver(98)     
